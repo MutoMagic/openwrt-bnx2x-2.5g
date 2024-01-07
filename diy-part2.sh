6b88952ae8b8aca139f4626a7282c3b6a3a8dec1
MutoMagic <8569775+MutoMagic@users.noreply.github.com>
2024-01-07 22:02:12 +0800

    path bug fixed

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-mv target openwrt/target
+mv $GITHUB_WORKSPACE/target target
 
-wget https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/llvm-bpf-15.0.7.Linux-x86_64.tar.xz -P openwrt
-tar -xvaf openwrt/llvm-bpf-15.0.7.Linux-x86_64.tar.xz
+wget https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/llvm-bpf-15.0.7.Linux-x86_64.tar.xz
+tar -xvaf llvm-bpf-15.0.7.Linux-x86_64.tar.xz
