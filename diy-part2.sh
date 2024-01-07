ca11fabde2798e1fab257d1fe6af1b6d40135c83
muto <8569775+MutoMagic@users.noreply.github.com>
2024-01-07 22:42:14 +0800

    mv -> cp

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-mv $GITHUB_WORKSPACE/target target
+cp -r $GITHUB_WORKSPACE/target target
+rm -r $GITHUB_WORKSPACE/target
 
 wget https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/llvm-bpf-15.0.7.Linux-x86_64.tar.xz
 tar -xvaf llvm-bpf-15.0.7.Linux-x86_64.tar.xz
