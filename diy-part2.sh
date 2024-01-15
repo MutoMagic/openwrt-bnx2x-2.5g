cf1c544911d3f6a6823a65f9e01ead4f6d14b782
MutoMagic <8569775+MutoMagic@users.noreply.github.com>
2024-01-15 18:19:52 +0800

    硬件升级后不再使用预编译llvm-bpf

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,3 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 cp -rf $GITHUB_WORKSPACE/target target
 rm -rf $GITHUB_WORKSPACE/target
-
-wget https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/llvm-bpf-15.0.7.Linux-x86_64.tar.xz
-tar -xvaf llvm-bpf-15.0.7.Linux-x86_64.tar.xz
